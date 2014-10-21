72bb6c41935aa8457ebc29351e6bb54a76f498a6
Harshit Chopra <harshit.chopra.19@gmail.com>
2014-10-21 16:38:51 +0530

    Complete function for rectangular flow and improved semantics

--- integration.m
+++ integration.m
@@ -1,3 +1,9 @@
+% Create function for integration
+
+function[flow_rate] = integration(type)
+%Call either sub-function depending on type
+end
+
 % Creating sub-function to determine Flow rate for circular cross section
 
 function[actual_flow_rate] = cicural_integration(vmax, radius, qdiff)
@@ -15,8 +21,12 @@ end
 function[actual_flow_rate] = rectangular_integration(vmax, x, y, qdiff)
 
 Width = 4.875                 %Width of the canal
-height = 7.437                %Height of the canal
-n=80
-m=80
+Height = 7.437                %Height of the canal
+n=80                          %exponent in x direction
+m=80                          %exponent in y direction
 vrad = vmax.*(power(1-(2.*x)./Width).^(1./n)).*(power(1-(2.*y)./Height).^(1./m));
+
+qref = vmax.*Width.*Height.*m.*n./((1+m).*(1+n));
+
+actual_flow_rate = qref - qdiff;
 end
