94ede3907e01a6e885b7652a1c6987916dd93ad4
Harshit Chopra <harshit.chopra.19@gmail.com>
2014-10-21 15:32:39 +0530

    Add velocity equation for rectangular cross section

--- integration.m
+++ integration.m
@@ -1,4 +1,4 @@
-% Creating function to determine Flow rate for circular cross section
+% Creating sub-function to determine Flow rate for circular cross section
 
 function[actual_flow_rate] = cicural_integration(vmax, radius, qdiff)
 
@@ -7,4 +7,16 @@ vrad = @(r) vmax.*(power(1-(r./rad)).^(1./9));
 qref = integral(vrad, 0, rad);
 
 actual_flow_rate = qref-qdiff;
+end
 
+
+% Sub-function to determine Flow-rate for rectangular cross section
+
+function[actual_flow_rate] = rectangular_integration(vmax, x, y, qdiff)
+
+Width = 4.875                 %Width of the canal
+height = 7.437                %Height of the canal
+n=80
+m=80
+vrad = vmax.*(power(1-(2.*x)./Width).^(1./n)).*(power(1-(2.*y)./Height).^(1./m));
+end
